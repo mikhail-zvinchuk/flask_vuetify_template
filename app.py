74710d53e8f0dd8d770efc118bc03ba0a3655549
Mikhail Zvinchuk <91047014+mikhail-zvinchuk@users.noreply.github.com>
2024-10-25 18:58:41 +0400

    Adding ui
    Since we are going to publish from repo,  all the configs and distros are commited

--- app.py
+++ app.py
@@ -1,12 +1,23 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, send_from_directory
 from flask_cors import CORS
 
 # instantiate the app
-app = Flask(__name__)
+app = Flask(__name__, static_folder="ui/dist", static_url_path="/")
 app.config.from_object(__name__)
 
 # enable CORS
 CORS(app, resources={r"/*": {"origins": "*"}})
+
+
+@app.route("/heartbeat")
+def heartbeat():
+    return jsonify({"status": "healthy"})
+
+
+@app.route("/", defaults={"path": ""})
+@app.route("/<path:path>")
+def catch_all(path):
+    return app.send_static_file("index.html")
 
 
 # sanity check route
